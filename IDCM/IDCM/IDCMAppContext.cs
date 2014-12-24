e0eea4857822f1987b550b9867da0e4852ed3c59
JiahaiWu <jiahai.wu@foxmail.com>
2014-12-24 22:53:41 +0800

    Add IDCMExpress Sub Packages A1

---
+++ IDCM/IDCM/IDCMAppContext.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Windows.Forms;
+/********************************
+ * Individual Data Center of Microbial resources (IDCM)
+ * A desktop software package for microbial resources researchers.
+ * 
+ * Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+ * 
+ * @Contact NO.1 Beichen West Road, Chaoyang District, Beijing 100101, Email: office@im.ac.cn
+ */
+namespace IDCM
+{
+    /// <summary>
+    /// The class that handles the creation of the application windows
+    /// 本类作为IDCM的窗口任务、后台任务、线程任务的创建管理类。
+    /// 本实例只允许一次有效实例化，启动初始化窗口线程和心跳检测任务。
+    /// 心跳检测任务会定期地判断当前进程是否活动任务为空，准备退出。
+    /// @author JiahaiWu 2014-10
+    /// 基础初始化操作包含三个部分：
+    /// 1.工作空间独享验证checkWorkSpaceSingleton()
+    /// 2.基本数据库初始化验证 startForm()
+    /// 3.载入IDCM主窗口对象实例
+    /// </summary>
+    class IDCMAppContext : ApplicationContext
+    {
+        /// <summary>
+        /// 初始化应用程序上下文环境，并触发程序主界面启动的事件过程。
+        /// 说明：
+        /// 1. 同一个工作空间下本实例进程要求排他性，如果已存在其他进程占用则执行退出过程。
+        /// 2. IDCMFormManager为用户交互界面主控管理器实现，该实例对象保持唯一性；
+        /// 3. 通过后台任务轮询监视器monitor来识别当前应用是否需要自动销毁进程资源，
+        /// 对于长驻线程任务实现应当注册到LongTermHandleNoter实例中，以标志当前线程任务具有事务保护性。
+        /// 由此实现的程序完全退出机制不受单一用户界面的关闭影响，强化事务完整性和数据操作完整性。
+        /// 在诸项事务队列完成之后，才能有效退出。
+        /// 4. 任务轮询监视器monitor的监听时间默认为2s。
+        /// @author JiahaiWu 2014-12
+        /// </summary>
+        /// <param name="workspacePath">目标工作空间路径（可选）</param>
+        public IDCMAppContext(string workspacePath = null)
+        {
+            if (!hasInited)
+            {
+                hasInited = true;
+                // Handle the ApplicationExit event to know when the application is exiting.
+                Application.ApplicationExit += new EventHandler(this.OnApplicationExit);
+                //初始化检测...
+                //检查当前目录下的进程实例是否已存在，如果存在执行退出操作;
+                //检查目标工作空间的文档是否已占用，如果被占用则执行退出操作;
+                WorkSpaceHolder.checkWorkSpaceSingleton(workspacePath);
+
+                // Create main application form and active the initForm method
+                mainManger = new IDCMFormManger();
+                mainManger.initForm(true);
+                LongTermHandleNoter.checkForIdle();
+                //Run HandleInstanceMonitor
+                monitor.Interval = 2000;
+                monitor.Tick += OnHeartBreak;
+                monitor.Start();
+            }
+        }
+        /// <summary>
+        /// 应用程序常规退出触发事件处理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnApplicationExit(object sender, EventArgs e)
+        {
+            log.Info("On IDCM application exiting.");
+        }
+
+        /// <summary>
+        /// 后台任务轮询监视器的心跳检测事件处理
+        /// 说明：
+        /// 当长驻线程任务实例悉数完成或中断时，满足IDCM应用进程退出的条件时立即销毁进程资源操作。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnHeartBreak(object sender, EventArgs e)
+        {
+#if DEBUG
+            //Console.WriteLine("* Heart Break For checkForIdle()");
+#endif
+            if (LongTermHandleNoter.checkForIdle())
+            {
+                monitor.Stop();
+                ExitThread();
+                this.Dispose();
+            }
+        }
+        /// <summary>
+        /// 用于共享成员实例获取当前用户交互界面主控管理器
+        /// 注意：
+        /// 该静态方法获取实例对象可能为空，调用方法需负责检测获取实例失败的特例情形。
+        /// </summary>
+        internal static IDCMFormManger MainManger
+        {
+            get
+            {
+                if (hasInited)
+                    return IDCMAppContext.mainManger;
+                else
+                    return null;
+            }
+        }
+
+        private static NLog.Logger log = NLog.LogManager.GetCurrentClassLogger();
+
+        /// <summary>
+        /// HandleInstanceMonitor
+        /// </summary>
+        private static System.Windows.Forms.Timer monitor = new System.Windows.Forms.Timer();
+        private static volatile bool hasInited = false;
+        private static IDCMFormManger mainManger = null;
+    }
+}
--- IDCM/IDCM/Program.cs
+++ IDCM/IDCM/Program.cs
@@ -2,20 +2,80 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
-
+/********************************
+ * Individual Data Center of Microbial resources (IDCM)
+ * A desktop software package for microbial resources researchers.
+ * 
+ * Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+ * 
+ * @Contact NO.1 Beichen West Road, Chaoyang District, Beijing 100101, Email: office@im.ac.cn
+ */
 namespace IDCM
 {
     static class Program
     {
         /// <summary>
-        /// 应用程序的主入口点。
+        /// IDCM应用程序的主入口点。
+        /// 说明：
+        /// 1.应用程序支持参数请求，请求格式为 IDCM.exe [-Option1 arg1] [-Option2 arg2] ...，目前支持项有
+        ///   1） -ws ${worksSpacePath}
+        /// 2.组件依赖主要包括
+        ///   1）.Net framework 4.0
+        ///   2）Nlog
+        ///   3）NPOI
+        ///   4) SQLite
+        ///   5) Dapper
+        ///   6) Newtonsoft.Json
+        ///   7) CSharpTest.Net.Libray
+        ///   8) Nuit
+        /// 注意：
+        /// 无效参数请求将被忽略处理，参数选项开关大小写敏感。
+        /// 
+        /// @author JiahaiWu
         /// </summary>
+        /// <param name="args">应用命令调用请求附加参数</param>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                dynamic xargs = commandArgScreening(args);
+                Application.Run(new IDCMAppContext(xargs));
+            }
+            catch (Exception ex)
+            {
+#if DEBUG
+                MessageBox.Show("It's Crash! \n FATAL ERROR:" + ex.Message + "\n" + ex.StackTrace);
+#endif
+                log.Info("FATAL!", ex);
+            }
         }
+        /// <summary>
+        /// 控制台请求参数初筛
+        /// </summary>
+        /// <param name="args">应用程序启动时命令行参数</param>
+        /// <returns>返回过滤后的“有效”命令行参数表达</returns>
+        private static dynamic commandArgScreening(string[] args)
+        {
+            string ws = null;
+#if DEBUG
+            System.Diagnostics.Debug.Assert(args != null);
+#endif
+            if (args.Length > 0)
+            {
+                for (int i = 0; i < args.Length - 1; i++)
+                {
+                    if (args[i].Equals("-ws"))
+                    {
+                        ws = args[i + 1].Trim(new char[] { '"' });
+                    }
+                }
+            }
+            return ws;
+        }
+
+        private static NLog.Logger log = NLog.LogManager.GetCurrentClassLogger();
     }
 }
